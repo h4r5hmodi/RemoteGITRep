554d280316ac63771751c4df692b109c51a21340
harshDev1 <Dev1@gmail.com>
2017-04-29 19:27:31 +0530

    commit by Dev1

--- DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
+++ DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
@@ -3,7 +3,7 @@ package com.java.hm.gitdemo;
 public class DemoProg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello..GIT from Dev1");
 
 	}
 
