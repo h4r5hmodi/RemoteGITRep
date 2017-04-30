6648dd97d5cea11f405460b0c4ceb9323065ed21
harshDev1 <Dev1@gmail.com>
2017-04-30 18:53:57 +0530

    by dev1

--- DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
+++ DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
@@ -5,6 +5,7 @@ public class DemoProg {
 	public static void main(String[] args) {
 		System.out.println("Hello..GIT from Dev1");
 		System.out.println("Hello..GIT from TL-1");
+		System.out.println("Hello..GIT from Dev1-new");
 
 	}
 
