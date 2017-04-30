186b4269c75ea33e734ed353ed0fa478114a7e73
harshDev1 <Dev1@gmail.com>
2017-04-30 20:41:11 +0530

    changes by TL new Sum() method

---
+++ DemoGITProj/src/com/java/hm/gitdemo/ArithmeticOperation.java
@@ -0,0 +1,7 @@
+package com.java.hm.gitdemo;
+
+public class ArithmeticOperation {
+	public int sum(int x,int y){
+		return x+y;
+	}
+}
--- DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
+++ DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
@@ -5,7 +5,8 @@ public class DemoProg {
 	public static void main(String[] args) {
 		System.out.println("Hello..GIT from Dev1");
 		System.out.println("Hello..GIT from TL-1");
-
+		ArithmeticOperation artih_sum=new ArithmeticOperation();
+		System.out.println(artih_sum.sum(100, 200));
 	}
 
 }
