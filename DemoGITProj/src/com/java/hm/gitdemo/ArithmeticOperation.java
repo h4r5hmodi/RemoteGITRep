a8977c6f94c4cdb3239af5246a6299e3b1f5deb0
harshDev1 <Dev1@gmail.com>
2017-04-30 20:49:01 +0530

    new method created by dev1

--- DemoGITProj/src/com/java/hm/gitdemo/ArithmeticOperation.java
+++ DemoGITProj/src/com/java/hm/gitdemo/ArithmeticOperation.java
@@ -4,4 +4,7 @@ public class ArithmeticOperation {
 	public int sum(int x,int y){
 		return x+y;
 	}
+	public int sub(int a,int b){
+		return a-b;
+	}
 }
--- DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
+++ DemoGITProj/src/com/java/hm/gitdemo/DemoProg.java
@@ -5,8 +5,9 @@ public class DemoProg {
 	public static void main(String[] args) {
 		System.out.println("Hello..GIT from Dev1");
 		System.out.println("Hello..GIT from TL-1");
-		ArithmeticOperation artih_sum=new ArithmeticOperation();
-		System.out.println(artih_sum.sum(100, 200));
+		ArithmeticOperation artih=new ArithmeticOperation();
+		System.out.println(artih.sum(100, 200));
+		System.out.println(artih.sub(200, 100));//by dev1
 	}
 
 }
